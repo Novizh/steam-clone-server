5c7f7284c8955f0f7d7ea8ba0b745f37cfa96113
Novizh <novizhreload1@gmail.com>
2021-08-14 13:06:20 +0700

    register and login feature are working as intended

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,52 @@ mongoose.connect('mongodb://localhost:27017/steam-clone-db', {
     useUnifiedTopology: true
 });
 const User = require('../models/user');
+const { sign } = require('../helpers/jwt');
+const { hash, compare } = require('../helpers/bcrypt');
 
 class UserController {
+    static async register(request, response, next) {
+        try {
+            let user = await User.findOne({"email": request.body.email});
+            if (user) {
+                next({ code: 400, message: `This email has been registered!` });
+            } else {
+                let user = await User.findOne({"username": request.body.username});
+                if (user) {
+                    next({ code: 400, message: `This username has been taken!` });
+                } else {
+                    let newUser = {
+                        username: request.body.username,
+                        email: request.body.email,
+                        password: hash(request.body.password)
+                    }
+                    let user = await User.create(newUser);
+                    response.status(201).json({message: `User created successfully!`, user});
+                }
+            }
+        } catch (error) {
+            next({code:400, message: error.message});
+        }
+    }
+
+    static async login(request, response, next) {
+        try {
+            let user = await User.findOne({"username": request.body.username});
+            if (user) {
+                if (compare(request.body.password, user.password)) {
+                    let access_token = sign({id: user._id, username: user.username});
+                    response.status(200).json({message: `Login successful!`, access_token});
+                } else {
+                    next({ code: 400, message: 'Email or password incorrect!'});
+                }
+            } else {
+                next({ code: 400, message: 'Email or password incorrect!'});
+            }
+        } catch (error) {
+            next({code:400, message: error.message});
+        }
+    }
+
     static async getAll(request, response, next) {
         try {
             let users = await User.find({});
@@ -18,25 +62,10 @@ class UserController {
     static async getById(request, response, next) {
         try {
             let user = await User.findById(request.params.id);
-            response.status(200).json({user});
-        } catch (error) {
-            next({code:400, message: error.message});
-        }
-    }
-
-    static async create(request, response, next) {
-        try {
-            let user = await User.find({"email": request.body.email})
-            if (user.length === 1) {
-                next({ code: 400, message: `This email is already registered!` });
+            if (user) {
+                response.status(200).json({user});
             } else {
-                let newUser = {
-                    username: request.body.username,
-                    email: request.body.email,
-                    password: request.body.password
-                }
-                let user = await User.create(newUser);
-                response.status(201).json({message: `User created successfully!`, user});
+                next({code:404, message: `User not found!`});
             }
         } catch (error) {
             next({code:400, message: error.message});
@@ -45,7 +74,7 @@ class UserController {
 
     static async update(request, response, next) {
         try {
-            let user = await User.findById(request.params.id)
+            let user = await User.findById(request.params.id);
             if (user) {
                 let newUser = {
                     username: request.body.username,
@@ -64,9 +93,9 @@ class UserController {
 
     static async delete(request, response, next) {
         try {
-            let user = await User.findById(request.params.id)
+            let user = await User.findById(request.params.id);
             if (user) {
-                let user = await User.deleteOne({_id: request.params.id})
+                let user = await User.deleteOne({_id: request.params.id});
                 response.status(200).json({message: `User deleted successfully!`, user});
             } else {
                 next({code:404, message: `User not found!`});
--- seeds/userSeeder.js
+++ seeds/userSeeder.js
@@ -2,7 +2,6 @@ const MongoClient = require("mongodb").MongoClient;
 const { hash } = require('../helpers/bcrypt');
 
 async function seedDB() {
-    // Connection URL
     const uri = `mongodb://localhost:27017`;
     const client = new MongoClient(uri, {
         useNewUrlParser: true,
@@ -13,15 +12,15 @@ async function seedDB() {
         await client.connect();
         const collection = client.db("steam-clone-db").collection("users");
         
-        // The drop() command destroys all data from a collection.
         collection.drop();
-        // Create the first user
+        
         let user = {
             username: 'Novizh',
             email: 'novizh@dev.com',
             password: hash('password')
         }
         collection.insertOne(user);
+        
         console.log("Database seeded!");
     } catch (error) {
         console.log(error.stack);
