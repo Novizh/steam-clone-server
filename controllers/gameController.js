7f83581a28dfac2f87479dac83017a4e7befd1d5
Naufal <novuzhreload1@gmail.com>
2022-10-11 14:10:45 +0700

    Minor adjustment in error handling for all controllers

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,61 +1,73 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/steam-clone-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const { getDatabase } = require('../config/database');
 const Game = require('../models/game');
 
 class GameController {
     static async wishlist(request, response, next) {
         try {
-            let gameDetails = {
-                game_id: request.body.game_id,
-                imageUrl: request.body.imageUrl,
-                platforms: request.body.platforms.split(', '),
-                metascore: request.body.metascore,
-                title: request.body.title,
-                user_id: request.user.id
+            if (getDatabase()) {
+                let gameDetails = {
+                    game_id: request.body.game_id,
+                    imageUrl: request.body.imageUrl,
+                    platforms: request.body.platforms.split(', '),
+                    metascore: request.body.metascore,
+                    title: request.body.title,
+                    user_id: request.user.id
+                }
+                let whishlist = await Game.create(gameDetails);
+                response.status(201).json({ message: `Game added to wishlist successfully!`, whishlist });
+            } else {
+                console.error(`Can't connect to Cart database!`);
             }
-            let whishlist = await Game.create(gameDetails);
-            response.status(201).json({message: `Game added to wishlist successfully!`, whishlist});
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async getAllWishlist(request, response, next) {
         try {
-            let games = await Game.find({user_id: request.user.id});
-            response.status(200).json({games});
+            if (getDatabase()) {
+                let games = await Game.find({ user_id: request.user.id });
+                response.status(200).json({ games });
+            } else {
+                console.error(`Can't connect to Cart database!`);
+            }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async getById(request, response, next) {
         try {
-            let game = await Game.findById(request.params.id);
-            if (game) {
-                response.status(200).json({game});
+            if (getDatabase()) {
+                let game = await Game.findById(request.params.id);
+                if (game) {
+                    response.status(200).json({ game });
+                } else {
+                    throw { code: 404, message: `Game not found!` };
+                }
             } else {
-                next({code:404, message: `Game not found!`});
+                console.error(`Can't connect to Cart database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async removeWishlist(request, response, next) {
         try {
-            let game = await Game.findById(request.params.id);
-            if (game) {
-                let game = await Game.deleteOne({_id: request.params.id});
-                response.status(200).json({message: `Game removed from wishlist successfully!`, game});
+            if (getDatabase()) {
+                let game = await Game.findById(request.params.id);
+                if (game) {
+                    let game = await Game.deleteOne({ _id: request.params.id });
+                    response.status(200).json({ message: `Game removed from wishlist successfully!`, game });
+                } else {
+                    throw { code: 404, message: `Game not found!` };
+                }
             } else {
-                next({code:404, message: `Game not found!`});
+                console.error(`Can't connect to Cart database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 }
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,4 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/steam-clone-db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
+const { getDatabase } = require('../config/database');
 const User = require('../models/user');
 const { sign } = require('../helpers/jwt');
 const { hash, compare } = require('../helpers/bcrypt');
@@ -10,98 +6,122 @@ const { hash, compare } = require('../helpers/bcrypt');
 class UserController {
     static async register(request, response, next) {
         try {
-            let user = await User.findOne({"email": request.body.email});
-            if (user) {
-                next({ code: 400, message: `This email has been registered!` });
-            } else {
-                let user = await User.findOne({"username": request.body.username});
+            if (getDatabase()) {
+                let user = await User.findOne({ "email": request.body.email });
                 if (user) {
-                    next({ code: 400, message: `This username has been taken!` });
+                    throw { code: 400, message: `This email has been registered!` };
                 } else {
-                    let newUser = {
-                        username: request.body.username,
-                        email: request.body.email,
-                        password: hash(request.body.password)
+                    let user = await User.findOne({ "username": request.body.username });
+                    if (user) {
+                        throw { code: 400, message: `This username has been taken!` };
+                    } else {
+                        let newUser = {
+                            username: request.body.username,
+                            email: request.body.email,
+                            password: hash(request.body.password)
+                        }
+                        let user = await User.create(newUser);
+                        response.status(201).json({ message: `User created successfully!`, user });
                     }
-                    let user = await User.create(newUser);
-                    response.status(201).json({message: `User created successfully!`, user});
                 }
+            } else {
+                console.error(`Can't connect to the database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async login(request, response, next) {
         try {
-            let user = await User.findOne({"username": request.body.username});
-            if (user) {
-                if (compare(request.body.password, user.password)) {
-                    let access_token = sign({id: user._id, username: user.username});
-                    response.status(200).json({message: `Login successful!`, access_token});
+            if (getDatabase()) {
+                let user = await User.findOne({ "username": request.body.username });
+                if (user) {
+                    if (compare(request.body.password, user.password)) {
+                        let access_token = sign({ id: user._id, username: user.username });
+                        response.status(200).json({ message: `Login successful!`, access_token });
+                    } else {
+                        throw { code: 400, message: 'Email or password incorrect!' };
+                    }
                 } else {
-                    next({ code: 400, message: 'Email or password incorrect!'});
+                    throw { code: 400, message: 'Email or password incorrect!' };
                 }
             } else {
-                next({ code: 400, message: 'Email or password incorrect!'});
+                console.error(`Can't connect to Cart database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async getAll(request, response, next) {
         try {
-            let users = await User.find({});
-            response.status(200).json({users});
+            if (getDatabase()) {
+                let users = await User.find({});
+                response.status(200).json({ users });
+            } else {
+                console.error(`Can't connect to Cart database!`);
+            }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async getById(request, response, next) {
         try {
-            let user = await User.findById(request.params.id);
-            if (user) {
-                response.status(200).json({user});
+            if (getDatabase()) {
+                let user = await User.findById(request.params.id);
+                if (user) {
+                    response.status(200).json({ user });
+                } else {
+                    throw { code: 404, message: `User not found!` };
+                }
             } else {
-                next({code:404, message: `User not found!`});
+                console.error(`Can't connect to Cart database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async update(request, response, next) {
         try {
-            let user = await User.findById(request.params.id);
-            if (user) {
-                let newUser = {
-                    username: request.body.username,
-                    email: request.body.email,
-                    password: request.body.password
+            if (getDatabase()) {
+                let user = await User.findById(request.params.id);
+                if (user) {
+                    let newUser = {
+                        username: request.body.username,
+                        email: request.body.email,
+                        password: request.body.password
+                    }
+                    let user = await User.updateOne({ _id: request.params.id }, newUser);
+                    response.status(200).json({ message: `User updated successfully!`, user });
+                } else {
+                    throw { code: 404, message: `User not found!` };
                 }
-                let user = await User.updateOne({_id: request.params.id}, newUser);
-                response.status(200).json({message: `User updated successfully!`, user});
             } else {
-                next({code:404, message: `User not found!`});
+                console.error(`Can't connect to Cart database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 
     static async delete(request, response, next) {
         try {
-            let user = await User.findById(request.params.id);
-            if (user) {
-                let user = await User.deleteOne({_id: request.params.id});
-                response.status(200).json({message: `User deleted successfully!`, user});
+            if (getDatabase()) {
+                let user = await User.findById(request.params.id);
+                if (user) {
+                    let user = await User.deleteOne({ _id: request.params.id });
+                    response.status(200).json({ message: `User deleted successfully!`, user });
+                } else {
+                    throw { code: 404, message: `User not found!` };
+                }
             } else {
-                next({code:404, message: `User not found!`});
+                console.error(`Can't connect to Cart database!`);
             }
         } catch (error) {
-            next({code:400, message: error.message});
+            next(error);
         }
     }
 }
